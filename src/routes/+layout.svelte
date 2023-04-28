6123d11ee88ca2cb11992d198d43b8b483717925
xazhm <azzamhilme@gmail.com>
2023-04-28 23:01:03 +0700

    uploading...

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	
+// Your selected Skeleton theme:
+import '@skeletonlabs/skeleton/themes/theme-modern.css';
+
+// This contains the bulk of Skeletons required styles:
+import '@skeletonlabs/skeleton/styles/all.css';
+
+// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+import '../app.postcss';
+import '../app.css';
+
+import { AppShell } from '@skeletonlabs/skeleton';
+import { AppBar } from '@skeletonlabs/skeleton';
+
+</script>
+<!-- App Shell -->
+<AppShell>
+
+	<svelte:fragment slot="header"><AppBar><h1>WELCOME TO MY BLOG</h1></AppBar></svelte:fragment>
+	<!-- Router Slot -->
+
+	<slot />
+	<!-- ---- / ---- -->
+	<br>
+	<svelte:fragment slot="pageFooter">Dibuat Oleh Muhammad Azzam Hilmy</svelte:fragment>
+
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+	
+// Your selected Skeleton theme:
+import '@skeletonlabs/skeleton/themes/theme-modern.css';
+
+// This contains the bulk of Skeletons required styles:
+import '@skeletonlabs/skeleton/styles/all.css';
+
+// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+import '../app.postcss';
+import '../app.css';
+
+import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+
+</script>
+
+<br>
+<div class="box px-24">
+<div class="card p-12 shadow-lg shadow-white">
+			 <ol class="breadcrumb">
+	<li><span class="badge variant-filled">Home</span></li>
+	<li class="crumb-separator" aria-hidden>/</li>
+	<li><span class="badge variant-filled">Blog</span></li>
+	<li class="crumb-separator" aria-hidden>/</li>
+	<li><span class="badge variant-filled">Article</span></li>
+</ol>
+<br>
+			               <h2>CARA MEMBUAT JASUKE<br>[COCOK UNTUK BERBUKA PUASA]</h2>
+    	<br><header class="card-header block card card-hover p-4">
+	<center>
+			<img alt="" src="https://blog.tokowahab.com/wp-content/uploads/2018/08/resep-jasuke.jpg">
+	</center>
+		</header>
+	<!-- <header class="card-header">ICIKIWIR</header> -->
+	<section class="p-4">
+		<center>[JASUKE - Jagung Susu Kedelai]</center><br>
+		<header class="card-header block card card-hover p-4">
+		Jagung Susu Kedelai atau JASUKE adalah minuman sehat yang terbuat dari campuran jagung, susu, dan kedelai. Kombinasi unik ini menghasilkan minuman dengan rasa yang nikmat serta kaya akan nutrisi. Jagung kaya akan serat, sementara kedelai kaya akan protein dan kalsium, dan susu mengandung vitamin D dan kalsium.
+	</header>
+	</section>
+	
+	<footer class="card-footer"><center>Cara membuat JASUKE:</center></footer>
+
+<Accordion>
+	<AccordionItem>
+		<svelte:fragment slot="lead"><span class="badge bg-primary-500">></span></svelte:fragment>
+		<svelte:fragment slot="summary">STEP 1</svelte:fragment>
+		<svelte:fragment slot="content">
+			               Siapkan bahan-bahan (jagung manis, margarin, keju parut, dan susu kental manis) lalu kukus jagung sampai empuk. Kemudian tambahkan margarin, susu, dan keju parut.
+		</svelte:fragment>
+	</AccordionItem>
+	<AccordionItem>
+		<svelte:fragment slot="lead"><span class="badge bg-primary-500">></span></svelte:fragment>
+				<svelte:fragment slot="summary">STEP 2</svelte:fragment>
+		<svelte:fragment slot="content">
+			               Setelah semua tercampur rata dan margarin meleleh, aduk rata.
+
+		</svelte:fragment>
+	</AccordionItem>
+
+		<AccordionItem>
+		<svelte:fragment slot="lead"><span class="badge bg-primary-500">></span></svelte:fragment>
+				<svelte:fragment slot="summary">STEP 3</svelte:fragment>
+		<svelte:fragment slot="content">
+               Sajikan di atas mangkuk dengan taburan keju sesuai selera.
+
+		</svelte:fragment>
+	</AccordionItem>
+	<section class="p-4">
+<header class="card-header block card card-hover p-4">
+            Begitulah cara pembuatan dari JASUKE. Nikmati segarnya JASUKE sebagai teman Anda di kala santai atau sebagai tambahan nutrisi sehat untuk tubuh Anda.
+	</header>
+	</section>
+    
+    <!-- ... -->
+</Accordion>
+
+
+
+</div>
+</div>
+
+				
